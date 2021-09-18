987fa0f381a45fefabb3d2f2fc2a75dad79dcb33
Dmitry Kabanov <dkabanov6@gmail.com>
2021-09-18 16:27:01 +0500

    added helper form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,46 +1,10 @@
 # frozen_string_literal: true
 
 require 'hexlet_code/version'
-
-def make_attributes(attributes)
-  attributes.to_a.map { |(key, value)| "#{key}='#{value}'" }.join ' '
-end
-
-def build_br(attributes)
-  "<br #{attributes}>"
-end
-
-def build_img(attributes)
-  "<img #{attributes}>"
-end
-
-def build_input(attributes)
-  "<input #{attributes}>"
-end
-
-def build_label(attributes, &body)
-  "<label #{attributes}>#{body.call}</label>"
-end
+require 'hexlet_code/helpers'
 
 module HexletCode
+  extend FormHelper
+
   class Error < StandardError; end
-
-  module Tag
-    def self.build(tag_name, attributes, &body)
-      attr = make_attributes attributes
-
-      builders = {
-        br: -> { build_br attr },
-        img: -> { build_img attr },
-        input: -> { build_input attr },
-        label: -> { build_label attr, &body }
-      }
-
-      builders[tag_name.to_sym].call
-    end
-  end
-
-  def self.form_for(url: '#')
-    "<form action='#{url}' method='post'></form>"
-  end
 end
---
+++ lib/hexlet_code/helpers.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Creates form with inputs
+module FormHelper
+  class Form
+    def initialize(struct)
+      @struct = struct
+    end
+
+    def input(field, as:, collection:)
+      p field
+      p as
+      p collection
+    end
+  end
+
+  def form_for(struct, url: '#', &block)
+    form = Form.new struct
+    "<form action='#{url}' method='post'>#{block.call form}</form>"
+  end
+end
