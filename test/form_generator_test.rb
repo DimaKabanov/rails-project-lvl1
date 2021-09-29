61b64680d0f3feebf7f13a6d036501a9a09f5800
Dmitry Kabanov <dkabanov6@gmail.com>
2021-09-29 14:54:44 +0500

    added new tests

--- test/form_generator_test.rb
+++ test/form_generator_test.rb
@@ -9,19 +9,43 @@ end
 
 class FormGeneratorTest < Minitest::Test
   def setup
-    user = Struct.new(:name, :job, :gender, keyword_init: true)
-    @user = user.new name: 'rob', job: 'hexlet', gender: 'm'
+    user = Struct.new(:name, :email, :job, :gender, keyword_init: true)
+    @user = user.new name: 'rob', email: 'test@mail.com', job: 'hexlet', gender: 'm'
   end
 
   def test_render_form
     actual_form = HexletCode.form_for @user do |f|
-      f.input :name
+      f.input :name, class: 'input-text'
+      f.input :email, type: :email
       f.input :job, as: :text
       f.input :gender, as: :select, collection: %w[m f]
       f.submit
     end
 
-    html_path = get_fixture_path('index.html')
+    html_path = get_fixture_path('form.html')
+    expected_form = File.read(html_path)
+
+    assert_equal actual_form, expected_form
+  end
+
+  def test_render_form_without_types
+    actual_form = HexletCode.form_for @user do |f|
+      f.input :name
+      f.submit
+    end
+
+    html_path = get_fixture_path('form_without_types.html')
+    expected_form = File.read(html_path)
+
+    assert_equal actual_form, expected_form
+  end
+
+  def test_render_custom_submit
+    actual_form = HexletCode.form_for @user do |f|
+      f.submit 'Submit'
+    end
+
+    html_path = get_fixture_path('custom_submit.html')
     expected_form = File.read(html_path)
 
     assert_equal actual_form, expected_form
